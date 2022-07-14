8eab005358011918722f2837f374d87a861f9eab
Vu <alexander.vu@goldlnk.rootlnka.net>
2022-07-13 22:28:01 -0700

    Update grid

--- foundations/projects/etch-a-sketch/scripts/etch-a-sketch.js
+++ foundations/projects/etch-a-sketch/scripts/etch-a-sketch.js
@@ -1,25 +1,35 @@
 function createGrid(numOfTiles = 16) {
     const containerDiv = document.querySelector('div#container')
-    let newRowDiv = null;
     let newColumnDiv = null;
+    let newBoxDiv = null;
 
     // Create the grid rows and columns
     for (let row = 0; row < numOfTiles; row++) {
-        newRowDiv = document.createElement('div');
-        newRowDiv.classList.add('grid=row');
+        newColumnDiv = document.createElement('div');
+        newColumnDiv.classList.add('grid-column');
 
         for (let column = 0; column < numOfTiles; column++) {
-           newColumnDiv = document.createElement('div');
-            newColumnDiv.classList.add('grid-column', 'grid-box');
-            newRowDiv.appendChild(newColumnDiv);
+            newBoxDiv = document.createElement('div');
+            newBoxDiv.classList.add('grid-box');
+            newColumnDiv.appendChild(newBoxDiv);
         }
 
-        containerDiv.appendChild(newRowDiv);
+        containerDiv.appendChild(newColumnDiv);
     }
+}
+
+function colorBox(color = "grey") {
+    const gridBoxes = document.querySelectorAll('div.grid-box');
+    gridBoxes.forEach((box) => {
+        box.addEventListener('mouseover', (e) => {
+            box.style.backgroundColor = color;
+        })
+    })
 }
 
 function main() {
     createGrid();
+    colorBox();
 }
 
 main();
